7dd8e1ea73b8229bf88cbe507eb00c0de08a825d
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-12 13:25:49 -0700

    Add error handling to validations

--- market/routes.py
+++ market/routes.py
@@ -17,6 +17,7 @@ def market_page():
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
@@ -24,4 +25,9 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+    
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
+
     return render_template('register.html', form=form)
