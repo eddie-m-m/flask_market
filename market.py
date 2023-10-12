2c0fc2f245aec29def4a9d0e1525d6d58922ad02
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-11 18:05:11 -0700

    Add item model

--- market.py
+++ market.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
-class Item 
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 @app.route('/')
 @app.route('/home')
